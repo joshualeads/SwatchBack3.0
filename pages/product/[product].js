53fc8244031f99a72cf7876065b2ffa7c72a8380
joshuajabakumar <joshua.jabakumar@bp.com>
2023-02-24 16:45:07 +0530

    Build Fix: ProductDetail Page

--- pages/product/[product].js
+++ pages/product/[product].js
@@ -108,7 +108,7 @@ const ProductDetailPage = ({product}) => {
 
               {/* Related Products */}
               {
-                product.recomendedProducts.references.edges ?
+                product.recomendedProducts ?
                   (
                     product.recomendedProducts.references.edges.map((prod)=>{
                       return <RelatedProducts key={prod.node.id}
